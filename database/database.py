ea2908e9252de5b5bc6d588a290955fb0000dd57
Mailstorm-ctrl <53045430+Mailstorm-ctrl@users.noreply.github.com>
2019-09-13 14:27:28 -0400

    Update database.py
    Added pool connections
    Added more query for backend functions
    Maybe improved queries?

--- database/database.py
+++ database/database.py
@@ -1,20 +1,6 @@
 #import asyncpg
+#import asyncio
 import time
-import asyncio
-from os.path import join, dirname
-from dotenv import load_dotenv
-
-#Role: Thread-bot
-# U
-# Need 2 tables
-#
-# GUILD_CONFIGURATION TABLE:
-#
-#     Guild_id, prefix, threads_category_id
-#
-# THREAD TABLE:
-#
-#     Thread_id (auto-increment int), guild_id, channel_id, op_message_id, created_by_id, thread_author_id, last_message_time
 
 class database:
 
@@ -22,53 +8,91 @@ class database:
         self.con = con
 
     async def joined_guild(self, guild, category):
-        await self.con.execute(f"INSERT INTO guild_configuration(guild_id,prefix,threads_category_id) VALUES({guild.id},'!',{category.id});")
-        return
-
+        async with self.con.acquire() as con:
+            await con.execute(f"INSERT INTO guild_configuration(guild_id, prefix, threads_category_id) VALUES($1, $2, $3) ON CONFLICT (guild_id) DO UPDATE SET threads_category_id=$3;", guild.id, '.', category.id)
+    
     def get_prefix(self, guild):
         cur = self.con.cursor()
-        cur.execute(f'SELECT prefix FROM guild_configuration WHERE guild_id={guild.id};')
+        cur.execute(f'SELECT prefix FROM guild_configuration WHERE guild_id=%s;', (guild.id,))
         return cur.fetchone()
 
     async def update_prefix(self, guild, prefix):
-        await self.con.execute(f"UPDATE guild_configuration SET prefix='{prefix}' WHERE guild_id={guild.id};")
-        return
+        async with self.con.acquire() as con:
+            await con.execute(f"UPDATE guild_configuration SET prefix=$1 WHERE guild_id=$2;", prefix, guild.id)
 
     async def set_threads_category_id(self, guild, cid):
-        await self.con.execute(f'UPDATE guild_configuration SET threads_category_id={cid} WHERE guild_id={guild.id};')
-        return
+        async with self.con.acquire() as con:
+            await con.execute(f'UPDATE guild_configuration SET threads_category_id=$1 WHERE guild_id=$2;', cid, guild.id)
 
     async def create_thread(self, channel, author, msg):
-        await self.con.execute(f'INSERT INTO threads (guild_id, channel_id, created_by, thread_author) VALUES({channel.guild.id},{channel.id},{author.id},{msg.author.id});')
-        return True
+        async with self.con.acquire() as con:
+            await con.execute(f'INSERT INTO threads (guild_id, channel_id, created_by, thread_author, keep_thread) VALUES($1, $2, $3, $4, $5);', channel.guild.id, channel.id, author.id, msg.author.id, False)
 
-    async def update_thread_op(self, thread_id, op_message):
-        await self.con.execute(f'UPDATE threads SET op_message_id={op_message.id} WHERE thread_id={thread_id};')
+    async def update_custom_categories(self, categories, guild):
+        async with self.con.acquire() as con:
+            await con.execute('UPDATE guild_configuration SET custom_categories=$1::jsonb WHERE guild_id=$2', categories, guild.id)
 
-    async def update_last_message(self, k, v):
-        await self.con.execute(f'UPDATE threads SET last_message_time={v[1]} WHERE channel_id={k} AND guild_id={v[0]};')
-        return
+    async def update_channel_permissions_by_channel(self, channel, permissions):
+        async with self.con.acquire() as con:
+            await con.execute(f"UPDATE threads SET permission_values=$1::jsonb WHERE channel_id=$2 AND guild_id=$3;", permissions, channel.id, channel.guild.id)
 
-    async def delete_thread(self, thread_id):
-        await self.con.execute(f'DELETE FROM threads WHERE thread_id={thread_id};')
-        return
+    async def update_thread_op(self, thread, op_message):
+        async with self.con.acquire() as con:
+            await con.execute(f'UPDATE threads SET op_message_id=$1 WHERE channel_id=$2 AND guild_id=$3;', op_message.id, thread.id, thread.guild.id)
 
-    async def get_all_threads(self):
-        """Database query used to fill cache with all channel information for quicker lookups"""
-        return {row[0]:[row[1],row[2],row[3],row[4],row[5],row[6]] for row in await self.con.fetch('SELECT * FROM threads;')}
+    async def update_last_message(self, channel):
+        async with self.con.acquire() as con:
+            await con.execute(f"""UPDATE threads SET last_message_age=$1 WHERE channel_id=$2 AND guild_id=$3;""", round(time.time()), channel.id, channel.guild.id)
 
-    async def get_thread_category_ids(self):
-        cats = {}
-        q = await self.con.fetch(f'SELECT guild_id, threads_category_id FROM guild_configuration;')
-        for row in q:
-            cats[row[0]] = row[1]
-        return cats
+    async def update_keep_thread(self, channel, setting):
+        async with self.con.acquire() as con:
+            await con.execute("UPDATE threads SET keep_thread=$1 WHERE channel_id=$2 AND guild_id=$3;", setting, channel.id, channel.guild.id)
+
+    async def delete_thread_old(self):
+        async with self.con.acquire() as con:
+            await con.execute("DELETE FROM threads WHERE last_message_age < (extract(epoch from now())-432000) AND keep_thread=FALSE;")
+
+    async def delete_threads(self, thread):
+        async with self.con.acquire() as con:
+            await con.execute('DELETE FROM threads WHERE channel_id=$1 and guild_id=$2', thread.id, thread.guild.id)
+
+    async def get_thread_info_by_channel_id(self, channel):
+        async with self.con.acquire() as con:
+            return [[row[0], row[1],row[2],row[3],row[4],row[5],row[6],row[7]] for row in await con.fetch(f'SELECT * FROM threads WHERE channel_id=$1 AND guild_id=$2;', channel.id, channel.guild.id)]
+    
+    async def get_permissions_for_channel(self, channel):
+        async with self.con.acquire() as con:
+            return [row[0] for row in await con.fetch(f'SELECT permission_values FROM threads WHERE channel_id=$1 AND guild_id=$2;', channel.id, channel.guild.id)]
+
+    async def get_thread_category_id(self, guild):
+        async with self.con.acquire() as con:
+            return [row[0] for row in await con.fetch(f'SELECT threads_category_id FROM guild_configuration WHERE guild_id=$1;', guild.id)]
+
+    async def get_custom_categories(self, guild):
+        async with self.con.acquire() as con:
+            return [row[0] for row in await con.fetch('SELECT custom_categories FROM guild_configuration WHERE guild_id=$1', guild.id)]
 
     async def get_thread_channels(self):
-        return {row[0]:row[1] for row in await self.con.fetch('SELECT channel_id, guild_id FROM threads')}
+        async with self.con.acquire() as con:
+            q = await con.fetch('SELECT DISTINCT guild_id FROM threads;')
+            cache = {record[0]:[] for record in await con.fetch('SELECT DISTINCT guild_id FROM threads;')}
+            for guild in cache:
+                q = await con.fetch('SELECT channel_id FROM threads WHERE guild_id=$1;', guild)
+                for channel in q:
+                    cache[guild].append(channel[0])
+            return cache
 
-    async def get_thread_id_from_channel(self, channel):
-        return [row[0] for row in await self.con.fetch(f'SELECT thread_id FROM threads WHERE guild_id={channel.guild.id} AND channel_id={channel.id};')]
+    async def get_guilds(self):
+        async with self.con.acquire() as con:
+            glist = [row[0] for row in await con.fetch('SELECT guild_id FROM guild_configuration;')]
+            return glist
 
-    async def get_last_messages(self):
-        return {row[0]:[row[1], row[2]] for row in await self.con.fetch(f'SELECT channel_id,guild_id,last_message_time FROM threads;')}
+    async def get_full_dead_channels(self):
+        async with self.con.acquire() as con:
+            dlist = [row[0] for row in await con.fetch("SELECT channel_id FROM threads WHERE last_message_age < (extract(epoch from now())-345600) AND keep_thread=FALSE;")]
+            return dlist
+
+    async def get_half_dead_channels(self):
+        async with self.con.acquire() as con:
+            dlist = [row[0] for row in await con.fetch("SELECT channel_id FROM threads WHERE last_message_age < (extract(epoch from now())-259200) AND last_message_age > (extract(epoch from now())-345600) AND keep_thread = FALSE;")]
+            return dlist
