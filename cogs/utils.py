6d2b2bece45ed2ac6a7e39764765983810df1f56
Mailstorm-ctrl <53045430+Mailstorm-ctrl@users.noreply.github.com>
2019-09-13 14:24:33 -0400

    Update utils.py
    Added integrity checking for guilds on bot startup
    Added command to initialize server in-case it was missed
    Improved looping for checking channels
    Improved channel checking for updating last message time

--- cogs/utils.py
+++ cogs/utils.py
@@ -10,25 +10,34 @@ class TB_Utils(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.channel_checker.start()
-        self.update_last_message.start()
+        self.integrity_check.start()
 
     @commands.Cog.listener()
-    async def on_guild_join(guild):
-        thread_category = discord.utils.get(guild.text_channels, name='Threads')
+    async def on_guild_join(self, guild):
+        thread_category = discord.utils.get(guild.categories, name='THREADS')
+        sql = database(self.bot.db)
         if thread_category is None:
-            try:
-                sql = database(self.bot.db)
-                category = await guild.create_category('Threads', reason='Initial category of threads creation.')
-                await database.joined_guild(guild, category)
-            except:
-                return
+            thread_category = await guild.create_category('THREADS', reason='Initial category of threads creation.')
+        await sql.joined_guild(guild, thread_category)
 
     @commands.Cog.listener()
     async def on_message(self, message):
         if message.guild is None:
             return
-        if not message.author.bot and message.channel.id in self.bot.thread_channels:
-            self.bot.last_message[message.channel.id] = [message.guild.id, int(time.time())]
+        if not message.author.bot:
+            if message.channel.id in self.bot.thread_check_cache.get(message.guild.id):
+                sql = database(self.bot.db)
+                await sql.update_last_message(message.channel)
+
+    @commands.command(name='setup', aliases=['tsetup'])
+    @commands.has_permissions(manage_guild=True)
+    async def create_guild_join(self, ctx):
+        thread_category = discord.utils.get(ctx.guild.categories, name='THREADS')
+        sql = database(self.bot.db)
+        if thread_category is None:
+            thread_category = await ctx.guild.create_category('THREADS', reason='Initial category of threads creation.')
+        await sql.joined_guild(ctx.guild, thread_category)
+        await ctx.send('Setup ran. Guild added to database.')
 
     @commands.command(name='prefix', aliases=['tprefix'])
     @commands.has_permissions(manage_roles=True)
@@ -38,85 +47,67 @@ class TB_Utils(commands.Cog):
         await ctx.send(f"Prefix updated to: {prefix}")
         return
 
-    @commands.command(name='source')
-    async def source_code(self, ctx):
-        embed=discord.Embed(
-                            title="GitHub page", 
-                            url="https://github.com/Mailstorm-ctrl/Thread-Bot", 
-                            description="Here you go, have a nice look at this butt nasty code.", 
-                            color=0xffffff
-                            )
-        embed.set_author(
-                        name=self.bot.user.name,
-                        icon_url=self.bot.user.avatar_url
-                        )
+    @commands.command(name='help')
+    async def thelp(self, ctx): # Yes I know bad. Use built-in help, don't hard-code yada
+        embed = await TB_Embeds.thelp(ctx)
         await ctx.send(embed=embed)
 
-    @tasks.loop(hours=12.0)
-    async def update_last_message(self):
+    @tasks.loop(count=1)
+    async def integrity_check(self):
         sql = database(self.bot.db)
-        for k,v in self.bot.last_message.items():
-            try:
-                await sql.update_last_message(k,v)
-            except:
-                continue
+        configured_guilds = await sql.get_guilds()
+        for guild in self.bot.guilds:
+            if guild.id not in configured_guilds:
+                try:
+                    category = await guild.create_category('Threads', reason='Initial category of threads creation.')
+                    await sql.joined_guild(guild, category)
+                except:
+                    continue
 
-    @update_last_message.before_loop
+    @integrity_check.before_loop
     async def wait2(self):
         await self.bot.wait_until_ready()
 
     @tasks.loop(hours=24.0)
     async def channel_checker(self):
+        """ Function loop that fetches channels that have been inactive for 3 to 4 days.
+        For channels that have had no activity for 3 days, go through and lock it.
+        For channels that have had no activity for 4 days, delete them."""
         sql = database(self.bot.db)
-        looper = copy.deepcopy(self.bot.all_channels)
-        for k,v in looper.items():
+        await sql.delete_thread_old()
+        channels = await sql.get_half_dead_channels()
+        for channel in channels:
             try:
-                var = self.bot.last_message.get(v[1])[1]
-                if (int(time.time()) - var) >= 259200:
-                    channel_found = True
-                    thread = self.bot.get_channel(v[1])
-                    if thread is None:
-                        try:
-                            thread = await self.bot.fetch_channel(v[1])
-                        except:
-                            channel_found = False
-                            self.bot.all_channels.pop(k,None)
-                            await sql.delete_thread(k)
-                    
-                    if channel_found:
-                        try:
-                            guild = self.bot.get_guild(v[0])
-                            if guild is None:
-                                guild = await self.bot.get_guild(v[0])
-                            embed = TB_Embeds.inactive_lock(guild)
-                            await thread.send(embed=embed)
-                        except:
-                            continue
-                        for role in thread.guild.roles:
-                            await thread.set_permissions(role,
-                                                        send_messages=False,
-                                                        reason='Inactive'
-                                                        )
-                
-                if (int(time.time()) - var) >= 345600:
-                    channel_found = True
-                    thread = self.bot.get_channel(v[1])
-                    if thread is None: #Channel not found in cache. Attempt to fetch it.
-                        thread = await self.bot.fetch_channel(v[1])
-                    
-                    if thread is None:
+                channel_found = True
+                thread_channel = self.bot.get_channel(channel)
+                if thread_channel is None:
+                    try:
+                        thread_channel = await self.bot.fetch_channel(channel)
+                    except:
                         channel_found = False
-                        self.bot.all_channels.pop(k,None)
-                        await sql.delete_thread(k)
-                    
-                    if channel_found:
-                        try:
-                            await thread.delete(reason='Inactivity')
-                            self.bot.all_channels.pop(k,None)
-                            await sql.delete_thread(k)
-                        except:
-                            continue
-            except:
+                if channel_found:
+                    embed = await TB_Embeds.inactive_lock(thread_channel)
+                    await thread_channel.send(embed=embed)
+                    for role in thread_channel.overwrites:
+                        await thread_channel.set_permissions(role, send_messages=False)
+            except Exception as e:
+                print(f'Exception in half_dead channels: {e}')
+                continue
+        
+        channels = await sql.get_full_dead_channels()
+        for channel in channels:
+            try:
+                channel_found = True
+                thread_channel = self.bot.get_channel(channel)
+                if thread_channel is None:
+                    try:
+                        thread_channel = await self.bot.fetch_channel(channel)
+                    except:
+                        channel_found = False
+                if channel_found:
+                    await thread_channel.delete(reason='Inactivity delete/cleanup')
+            except Exception as e:
+                print(f'Exception in full_dead channels: {e}')
                 continue
 
     @channel_checker.before_loop
