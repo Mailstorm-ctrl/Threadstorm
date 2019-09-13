e89a515cb31bc7cf9ac145e51a0a3b0048bbcfc0
Mailstorm-ctrl <53045430+Mailstorm-ctrl@users.noreply.github.com>
2019-09-13 14:28:04 -0400

    Add files via upload
    The file that gets executed

---
+++ thread_bot.py
@@ -0,0 +1,96 @@
+import discord
+import os
+import asyncpg
+import asyncio
+import psycopg2
+from database.database import database
+from os.path import join, dirname
+from dotenv import load_dotenv
+from discord.ext import commands
+
+# TODO:
+#       Let users change thread embed color
+#       Custom thread categories
+#       purge command
+#       Hide SQL table names via env
+#       #Final test
+
+def prefix_callable(bot_client, message):
+    try:
+        sql = database(bot_client.prefix)
+        prefix = sql.get_prefix(message.guild)
+        return prefix[0]
+    except:
+        return '.'
+
+bot = commands.Bot(command_prefix=prefix_callable, case_insensitive=True)
+bot.remove_command('help')
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as: {bot.user}")
+
+
+@bot.command()
+@commands.is_owner()
+async def reload_cog(ctx, cog):
+    bot.reload_extension(cog)
+    await ctx.send("Reload successful.")
+
+
+@bot.command(aliases=['invite', 'im', 'inviteme'])
+async def invite_me(ctx):
+    embed=discord.Embed(title="Invite Link",
+                        description=(
+                                    """[Click here to invite me to your server]
+                                    (https://discordapp.com/oauth2/authorize?client_id=617376702187700224&permissions=268561424&scope=bot).
+                                    \nPlease grant all the permissions I ask for. If you don't, I wont function correctly."""
+                                    ), 
+                                    color=0x0097ff
+                        )
+    embed.set_author(name=bot.user.name, icon_url=bot.user.avatar_url)
+    embed.set_thumbnail(url=bot.user.avatar_url)
+    await ctx.send(embed=embed)
+
+
+load_dotenv('thread_bot.env')
+bot_token = os.getenv('TOKEN')
+db = os.getenv('DATABASE')
+user = os.getenv('DB_USER')
+password = os.getenv('DB_PASS')
+
+DB_SETTINGS = {
+    'host': 'localhost',
+    'database': db,
+    'user': user,
+    'password': password,
+}
+
+cogs = ['cogs.makethread',
+        'cogs.editthread',
+        'cogs.lock_thread',
+        'cogs.embeds',
+        'cogs.utils', 
+        'cogs.create_category', 
+        'cogs.errors']
+
+
+for cog in cogs:
+    bot.load_extension(cog)
+
+
+if __name__ == '__main__':
+    db_loop = asyncio.get_event_loop()
+    bot.prefix = psycopg2.connect(**DB_SETTINGS)
+    bot.db = db_loop.run_until_complete(asyncpg.create_pool(**DB_SETTINGS, max_inactive_connection_lifetime=480))
+
+# Figure out how to get more than one record with asyncpg
+async def populate():
+     sql = database(bot.db)
+     bot.thread_check_cache = await sql.get_thread_channels()
+#     print(bot.thread_channels)
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(populate())
+
+bot.run(bot_token, reconnect=True)
