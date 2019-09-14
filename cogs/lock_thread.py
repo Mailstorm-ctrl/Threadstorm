848005c9a2e331226a13867de108cc5d07429b9e
Mailstorm-ctrl <53045430+Mailstorm-ctrl@users.noreply.github.com>
2019-09-14 14:35:19 -0400

    Update lock_thread.py
    Fixed bug for locking and unlocking regarding permissions

--- cogs/lock_thread.py
+++ cogs/lock_thread.py
@@ -12,7 +12,7 @@ class TB_Lock_Threads(commands.Cog):
         self.bot = bot
 
     async def is_thread(ctx):
-        if ctx.channel.id in bot.thread_check_cache.get(ctx.guild.id):
+        if ctx.channel.id in ctx.bot.thread_check_cache.get(ctx.guild.id):
             return True
         else:
             return False
@@ -26,12 +26,15 @@ class TB_Lock_Threads(commands.Cog):
         await ctx.send(embed=thread_locked_embed)
         sql = database(self.bot.db)
         thread_permissions = {role.id:[ctx.channel.overwrites.get(role).pair()[0].value, ctx.channel.overwrites.get(role).pair()[1].value] for role in ctx.channel.overwrites}
-        await sql.update_channel_permissions_by_channel(ctx.channel, json.dumps(thread_permissions))
-        for role in ctx.channel.overwrites:
-            if not role.permissions.manage_messages:
-                await ctx.channel.set_permissions(role, send_messages=False, reason=reason)
-            else:
-                continue
+        if len(thread_permissions) == 0:
+            await ctx.channel.set_permissions(ctx.guild.default_role, send_messages=False)
+        else:
+            await sql.update_channel_permissions_by_channel(ctx.channel, json.dumps(thread_permissions))
+            for role in ctx.channel.overwrites:
+                if not role.permissions.manage_messages and role != ctx.guild.default_role:
+                    await ctx.channel.set_permissions(role, send_messages=False, reason=reason)
+                else:
+                    continue
 
     @commands.command(name='unlock',aliases=['tunlock'])
     @commands.has_permissions(manage_messages=True)
@@ -40,12 +43,18 @@ class TB_Lock_Threads(commands.Cog):
         """The opposite of lock. Goes back through and assigns the original permissions to the channel."""
         sql = database(self.bot.db)
         thread_permissions = await sql.get_permissions_for_channel(ctx.channel)
-        thread_permissions = json.loads(thread_permissions[0])
-        for role_id in thread_permissions:
-            role = ctx.guild.get_role(int(role_id))
-            allow = discord.Permissions(permissions=thread_permissions.get(role_id)[0])
-            deny = discord.Permissions(permissions=thread_permissions.get(role_id)[1])
-            await ctx.channel.set_permissions(role, overwrite=discord.PermissionOverwrite().from_pair(allow,deny))
+        try:
+            thread_permissions = json.loads(thread_permissions[0])
+        except TypeError:
+            pass
+        if None in thread_permissions:
+            await ctx.channel.set_permissions(ctx.guild.default_role, send_messages=True)
+        else:
+            for role_id in thread_permissions:
+                role = ctx.guild.get_role(int(role_id))
+                allow = discord.Permissions(permissions=thread_permissions.get(role_id)[0])
+                deny = discord.Permissions(permissions=thread_permissions.get(role_id)[1])
+                await ctx.channel.set_permissions(role, overwrite=discord.PermissionOverwrite().from_pair(allow,deny))
         embed = await TB_Embeds.thread_unlocked(ctx.author)
         await ctx.send(embed=embed)
 
